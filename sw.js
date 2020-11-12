6faa02e4392c8faec6a3cfb0fa426d2cb7c3fa59
Doumeng <theodoum1@gmail.com>
2020-11-12 15:06:00 +0100

    Fetch

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-self.addEventListener('install', event => { event.waitUntil(Promise.resolve('Install phase succeed')); });
+self.addEventListener('message', event => {
+    this.clients.matchAll().then(clients => {
+        clients.forEach(client => client.postMessage('Enchanté, je suis le service worker'));
+    });
+});
+
+Self.addEventListener('fetch', event => {
+    event.RespondWith(newResponse('PWA!!!!'));
+});
+
+self.addEventListener('install', event => {
+    event.waitUntil(Promise.resolve('Install phase succeed'));
+});
