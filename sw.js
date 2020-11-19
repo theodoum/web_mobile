e519c7d862145ac0ae70c503754be4ca7ab768a0
Doumeng <theodoum1@gmail.com>
2020-11-19 09:13:41 +0100

    sw

--- sw.js
+++ sw.js
@@ -16,13 +16,3 @@ self.addEventListener('fetch', event => {
         );
     }
 });
-
-/*self.addEventListener('message', event => {
-    this.clients.matchAll().then(clients => {
-        clients.forEach(client => client.postMessage('Enchanté, je suis le service worker'));
-    });
-});
-
-self.addEventListener('install', event => {
-    event.waitUntil(Promise.resolve('Install phase succeed'));
-});*/
