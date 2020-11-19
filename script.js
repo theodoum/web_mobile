40fb6cfab7796f1bd70917d1b7f6da2fd66bd78f
Doumeng <theodoum1@gmail.com>
2020-11-19 10:03:20 +0100

    sw

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ function afficher(json) {
     });
     document.querySelector(".container").innerHTML = html;
 }
+
+if(navigator.online){
+    console.log("Vous êtes en mode en ligne !");
+}else if(navigator.offline){
+    console.log("Vous êtes en mode hors ligne !");
+}
+
+window.addEventListener('offline', event=>{
+    console.log("Vous venez de passer en mode hors ligne ! "+event);
+});
+
+window.addEventListener('online', event=>{
+    console.log("Vous venez de passer en mode en ligne ! "+event);
+});
