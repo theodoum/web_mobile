ee261a948aad3b582a5193d24d7ec704796e8621
Doumeng <theodoum1@gmail.com>
2020-11-19 09:00:35 +0100

    sw

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
+self.addEventListener('fetch', event => {
+    event.RespondWith(new Response('PWA!!!!'));
+});
+
 self.addEventListener('message', event => {
     this.clients.matchAll().then(clients => {
         clients.forEach(client => client.postMessage('Enchanté, je suis le service worker'));
     });
-});
-
-self.addEventListener('fetch', event => {
-    event.RespondWith(new Response('PWA!!!!'));
 });
 
 self.addEventListener('install', event => {
