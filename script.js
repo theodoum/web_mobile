2ed8fe36ce65affe39cf1ee591d5995df9a24d36
Doumeng <theodoum1@gmail.com>
2020-11-19 10:25:18 +0100

    sw

--- script.js
+++ script.js
@@ -39,3 +39,13 @@ self.addEventListener('install', function (event) {
             Returncache.addAll(['./index.html', './style.css', './script.js']);
         }));
 });
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.open('nom_du_cache')
+            .then(cache => cache.match(e.request))
+            .then(function (response) {
+                return response || fetch(e.request);
+            })
+    );
+});
