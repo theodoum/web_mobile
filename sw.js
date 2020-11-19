27f4dde6554981b7ce69dcbd1f94f0259bfc6963
Doumeng <theodoum1@gmail.com>
2020-11-19 09:11:48 +0100

    sw

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
 self.addEventListener('fetch', event => {
-    console.log(event.request.url);
+    const url = event.request.url;
+    if (url.indexOf("https://monNomDeDomaine/images.json") === 0) {
+        event.respondWith(
+            fetch(event.request).then((response) => {
+                if (response.statusText !== "OK") {
+                    console.error("Service Worker", "Error when fetching", event.request.url);
+                    return response;
+                }
+                console.info("Formatting data");
+                return response.json().then((json) => {
+                    const formattedResponse = json.map((j) => ({ name: j.name, description: j.description || "", updated_at: j.updated_at, }));
+                    return new Response(JSON.stringify(formattedResponse));
+                });
+            })
+        );
+    }
 });
 
 /*self.addEventListener('message', event => {
