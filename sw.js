606f212e10b8c85a937eb1a652a9463d13e0a741
theodoum <theodoum1@gmail.com>
2020-11-26 13:21:43 +0100

    update

--- sw.js
+++ sw.js
@@ -1,53 +1,73 @@
-self.addEventListener('fetch', event => {
-    const url = event.request.url;
-    if (url.indexOf("https://suspicious-pare-499c00.netlify.app/images.json") === 0) {
-        event.respondWith(
-            fetch(event.request).then((response) => {
-                if (response.statusText !== "OK") {
-                    console.error("Service Worker", "Error when fetching", event.request.url);
-                    return response;
-                }
-                console.info("Formatting data");
-                return response.json().then((json) => {
-                    const formattedResponse = json.map((j) => ({ name: j.name, description: j.description || "", updated_at: j.updated_at, }));
-                    return new Response(JSON.stringify(formattedResponse));
-                });
-            })
-        );
-    } else {
-        event.respondWith(
-            caches
-                .open(cacheName)
-                .then(cache => cache.match(event.request))
-                .then(response => response || fetch(url)));
-    }
-});
-
 const cacheName = "galerie";
+
 const files = [
-    "/",
-    "/script.js",
-    "https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.css",
-    "https://bulma.io/images/placeholders/1280x960.png",
-    "https://bulma.io/images/placeholders/96x96.png"
+  "/",
+  "/script.js",
+  "https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.css",
+  "https://bulma.io/images/placeholders/1280x960.png",
+  "https://bulma.io/images/placeholders/96x96.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/localforage/1.7.3/localforage.min.js"
 ];
 
 self.addEventListener("install", e => {
-    caches.open(cacheName).then(cache => {
-        cache.addAll(files);
-    });
+  caches.open(cacheName).then(cache => {
+    cache.addAll(files);
+  });
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
 
-self.addEventListener("activate", e => {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(
-                keyList.map(function (key) {
-                    if (key !== cacheName) {
-                        caches.delete(key);
-                    }
-                })
-            );
-        })
+self.addEventListener("fetch", e => {console.log(e.request.url);});
+
+self.addEventListener("fetch", event => {
+  const url = event.request.url;
+
+  if (url.indexOf("https://nostalgic-lamarr-5a666c.netlify.app/images.json") === 0) {
+    event.respondWith(
+      fetch(event.request).then(response => {
+        if (response.status === 200) {
+			console.info("Formatting data");
+			return response.json().then(json => {
+			const formattedResponse = json.map(j => ({
+			name: j.name,
+			description: j.description || "",
+			updated_at: j.updated_at
+			}));
+
+			return new Response(JSON.stringify(formattedResponse));
+			});
+        }
+		
+		else{
+			console.error(
+			"Service Worker",
+			"Error when fetching",			
+			event.request.url
+			);
+
+			return response;
+		}
+        
+      })
     );
+  } 
+  else {
+    event.respondWith(
+      caches
+        .open(cacheName)
+        .then(cache => cache.match(event.request))
+        .then(response => response || fetch(event.request))
+    );
+  }
 });
