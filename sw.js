3d7125ffcfd530b85fb0a0ad2fab23746265f5b0
theodoum <theodoum1@gmail.com>
2020-11-26 13:24:54 +0100

    update

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ self.addEventListener("fetch", e => {console.log(e.request.url);});
 self.addEventListener("fetch", event => {
   const url = event.request.url;
 
-  if (url.indexOf("https://nostalgic-lamarr-5a666c.netlify.app/images.json") === 0) {
+  if (url.indexOf("https://suspicious-pare-499c00.netlify.app//images.json") === 0) {
     event.respondWith(
       fetch(event.request).then(response => {
         if (response.status === 200) {
