5aae1d6865ecc12a3502a2c8141e8531006f9c4f
theodoum <theodoum1@gmail.com>
2020-12-03 09:41:29 +0100

    Mise à jour

---
+++ serveur/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+const port = 3000;
+
+let favoris = [];
+
+app.get("/favoris", (request, response) => {
+    response.send(favoris);
+});
+
+app.post("/favoris", (request, response) => {
+    console.log(request.body);
+    favoris = request.body;
+    response.send(favoris);
+});
+
+app.listen(port, err => {
+    console.log('server is listening on ${port}');
+});
